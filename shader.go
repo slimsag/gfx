97b0983c38c49c03a51b8873b5b542731bc5221b
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-27 16:03:59 -0700

    Add UniformLocation interface.

--- shader.go
+++ shader.go
@@ -1,5 +1,12 @@
 package gfx
 
+// UniformLocation corresponds to the location of a uniform variable in a
+// shader program.
+//
+// TODO(slimsag): consider simplification to e.g. integer
+type UniformLocation interface {
+}
+
 // Shader represents which content and how that content is drawn to the render
 // target. 
 type Shader interface {
