98225dec05fb6a691c914f0c063f4add639d9ab3
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-27 16:14:07 -0700

    Add Program.AttachShader method.

--- program.go
+++ program.go
@@ -3,4 +3,10 @@ package gfx
 // Program represents the programmable OpenGL pipeline and associated shader
 // programs.
 type Program interface {
+	// AttachShader attaches the given shader object to this program.
+	//
+	// If the shader is already attached to a program, or another shader of the
+	// same type is already attached to this program, ErrInvalidOperation is
+	// generated (see Context.Error).
+	AttachShader(s Shader) error
 }
