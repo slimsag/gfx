52ff460beea518356384e164242a7ec925d70e2c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-27 15:53:49 -0700

    Add Renderbuffer interface.

---
+++ renderbuffer.go
@@ -0,0 +1,10 @@
+package gfx
+
+// RenderBuffer represents a buffer which can contain an image and act as the
+// source or target of a render operation.
+//
+// It stores data that arepresents a single image. A Renderbuffer can be
+// attached to a Framebuffer and used to store either stencil data or a
+// combination of stencil and depth data. 
+type Renderbuffer interface {
+}
