1f38c19d1c1f836af7d9f5d1e78f6eddd66b6a77
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-27 16:52:15 -0700

    Add Framebuffer.Status method.

--- framebuffer.go
+++ framebuffer.go
@@ -1,6 +1,18 @@
 package gfx
 
+import "errors"
+
+var (
+	ErrFramebufferIncompleteAttachment        = errors.New("framebuffer: attachment types are mismatched")
+	ErrFramebufferIncompleteMissingAttachment = errors.New("framebuffer: missing attachment")
+	ErrFramebufferIncompleteDimensions        = errors.New("framebuffer: the width and height of the attachments are not the same")
+	ErrFramebufferUnsupported                 = errors.New("framebuffer: the attachments aren't supported")
+)
+
 // Framebuffer is a collection of buffers that serve as a rendering
 // destination.
 type Framebuffer interface {
+	// Status returns any framebuffer status error that might have occured. If
+	// nil is returned, the framebuffer is ready for display.
+	Status() error
 }
