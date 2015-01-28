7a72f7d8bdf1b1c3f2d567c30117abafd2bd7d26
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-28 14:51:27 -0700

    Document clearing more properly.

--- clearing.go
+++ clearing.go
@@ -3,35 +3,39 @@ package gfx
 // ClearColor is used by a Clearable framebuffer object to clear the color
 // buffer to a specific [R, G, B, A] color.
 //
-//  // Clear color buffer to red:
-//  Clear(gfx.ClearColor{1, 0, 0, 1})
+//  // Clear color buffer to it's initial color:
+//  Clear(gfx.ClearColor{0, 0, 0, 0})
 //
 //  // Equivilent OpenGL calls:
-//  glClearColor(1, 0, 0, 1)
+//  glClearColor(0, 0, 0, 0)
 //  glClear(GL_COLOR_BUFFER_BIT)
 //
+// All values are clamped to a range of [0, 1].
 type ClearColor [4]float32
 
 // ClearDepth is used by a Clearable framebuffer object to clear the depth
 // buffer to a specific stencil value.
 //
-//  // Clear depth buffer to zero:
-//  Clear(gfx.ClearDepth(0))
+//  // Clear depth buffer to it's initial value:
+//  Clear(gfx.ClearDepth(1))
 //
 //  // Equivilent OpenGL calls:
-//  glClearDepth(0)
+//  glClearDepth(1)
 //  glClear(GL_DEPTH_BUFFER_BIT)
 //
+// All values are clamped to a range of [0, 1].
 type ClearDepth float32
 
 // ClearStencil is used by a Clearable framebuffer object to clear the stencil
 // buffer to a specific stencil value.
 //
-//  // Clear stencil buffer to zero:
+//  // Clear stencil buffer to it's initial value:
 //  Clear(gfx.ClearStencil(0))
 //
 //  // Equivilent OpenGL calls:
 //  glClearStencil(0)
 //  glClear(GL_STENCIL_BUFFER_BIT)
 //
+// The value is masked with 2m-1, where m is the number of bits in the stencil
+// buffer.
 type ClearStencil int
