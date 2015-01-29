7a686d7b7a8737662b57512e5f92f576a3474be3
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-28 17:52:45 -0700

    debug: update for recent changes

--- debug/context.go
+++ debug/context.go
@@ -2,32 +2,44 @@ package debug
 
 import "github.com/slimsag/gfx"
 
-// Checker is a gfx.Context that implicitly invokes the Check method of the
-// underlying context after each function call made. Thus, if any error should
-// occur you will receive a nice stack trace where that error occured.
-//
-// It should not be used in production builds, as it has a performance
-// overhead.
-type Checker struct {
-	gfx.Context
+// checker is a gfx.Context that implicitly invokes the Check method of the
+// underlying context after each function call is made. Thus, if any error
+// should occur you will receive a nice stack trace where that error occured.
+type checker struct {
+	*fbChecker
+	ctx gfx.Context
 }
 
 // Check implements the gfx.Context interface.
-func (c Checker) Check() {
+func (c *checker) Check() {
 	// We don't want caller to accidently grab the error, so we stub out the
 	// call here.
 	return
 }
 
+// Checker wraps the given graphics context such that each function call to the
+// context (or any object gotten from it, e.g. a Framebuffer) has an implicit
+// Check() call after it.
+//
+// This ensures that, should any error occur in the context, you will receive
+// a nice Go stack trace with the exact function where the error was made.
+func Checker(c gfx.Context) gfx.Context {
+	return &checker{
+		fbChecker: &fbChecker{
+			fb:  c.(gfx.Framebuffer),
+			ctx: c,
+		},
+		ctx: c,
+	}
+}
+
 // Context wraps the given graphics context with a Checker, it is short-handed
 // for:
 //
-//  c = debug.Checker{
-//      Context: c,
-//  }
+//  c = debug.Checker(c)
 //
 func Context(c gfx.Context) gfx.Context {
-	return Checker{
-		Context: c,
-	}
+	return Checker(c)
 }
+
+// TODO(slimsag): check for invalid Framebuffer.Clear masks
---
+++ debug/framebuffer.go
@@ -0,0 +1,35 @@
+package debug
+
+import "github.com/slimsag/gfx"
+
+// fbChecker is like the checker type, but for a gfx.Framebuffer. It implicitly
+// invokes the Check method of the underlying context after each function call
+// is made.
+type fbChecker struct {
+	fb  gfx.Framebuffer
+	ctx gfx.Context
+}
+
+// ClearColor implements the gfx.Clearable interface.
+func (f *fbChecker) ClearColor(r, g, b, a float32) {
+	f.fb.ClearColor(r, g, b, a)
+	f.ctx.Check()
+}
+
+// ClearDepth implements the gfx.Clearable interface.
+func (f *fbChecker) ClearDepth(depth float32) {
+	f.fb.ClearDepth(depth)
+	f.ctx.Check()
+}
+
+// ClearStencil implements the gfx.Clearable interface.
+func (f *fbChecker) ClearStencil(stencil int) {
+	f.fb.ClearStencil(stencil)
+	f.ctx.Check()
+}
+
+// Clear implements the gfx.Clearable interface.
+func (f *fbChecker) Clear(m gfx.ClearMask) {
+	f.fb.Clear(m)
+	f.ctx.Check()
+}
