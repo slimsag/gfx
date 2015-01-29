9589ca528d93183943cef08dc7c0d8925f68f3c9
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-29 11:01:32 -0700

    gles2: add working OpenGL ES 2 implementation.

---
+++ gles2/gles2_context.go
@@ -0,0 +1,113 @@
+// Copyright 2015 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// +build arm gles2
+
+package gles2
+
+import (
+	"fmt"
+
+	"github.com/slimsag/gfx"
+	gl "github.com/slimsag/gfx/internal/gles2/2.0/gles2"
+)
+
+// Context implements the gfx.Context interface.
+type Context struct {
+	// The default framebuffer implementation for the context.
+	*Framebuffer
+
+	LastBindFramebuffer uint32
+	LastClearColor      [4]float32
+	LastClearDepth      float64
+	LastClearStencil    int
+}
+
+func (c *Context) fastBindFramebuffer(framebuffer uint32) {
+	if c.LastBindFramebuffer == framebuffer {
+		return
+	}
+	c.LastBindFramebuffer = framebuffer
+	gl.BindFramebuffer(gl.FRAMEBUFFER, framebuffer)
+}
+
+func (c *Context) fastClearColor(v [4]float32) {
+	if c.LastClearColor == v {
+		return
+	}
+	c.LastClearColor = v
+	gl.ClearColor(v[0], v[1], v[2], v[3])
+}
+
+func (c *Context) fastClearDepth(v float64) {
+	if c.LastClearDepth == v {
+		return
+	}
+	c.LastClearDepth = v
+	gl.ClearDepthf(float32(v))
+}
+
+func (c *Context) fastClearStencil(v int) {
+	if c.LastClearStencil == v {
+		return
+	}
+	c.LastClearStencil = v
+	gl.ClearStencil(int32(v))
+}
+
+// Check implements the gfx.Context interface.
+func (c *Context) Check() {
+	e := gl.GetError()
+
+	// Avoid the larger switch statement below, as no error is the most likely
+	// case.
+	if e == gl.NO_ERROR {
+		return
+	}
+
+	switch e {
+	case gl.OUT_OF_MEMORY:
+		panic(gfx.OutOfMemory)
+	case gl.INVALID_ENUM:
+		panic(gfx.InvalidEnum)
+	case gl.INVALID_OPERATION:
+		panic(gfx.InvalidOperation)
+	case gl.INVALID_FRAMEBUFFER_OPERATION:
+		panic(gfx.InvalidFramebufferOperation)
+	case gl.INVALID_VALUE:
+		panic(gfx.InvalidValue)
+	case gl.STACK_OVERFLOW:
+		panic(gfx.StackOverflow)
+	case gl.STACK_UNDERFLOW:
+		panic(gfx.StackUnderflow)
+	case gl.CONTEXT_LOST:
+		panic(gfx.ContextLost)
+	default:
+		panic(fmt.Sprintf("gl2: unhandled error 0x%X\n", e))
+	}
+}
+
+// Flush implements the gfx.Context interface.
+func (c *Context) Flush() {
+	gl.Flush()
+}
+
+// Finish implements the gfx.Context interface.
+func (c *Context) Finish() {
+	gl.Finish()
+}
+
+// New returns a new OpenGL ES 2 graphics context. It must only be called under
+// the presence of an active OpenGL context in the OS thread.
+func New() (gfx.Context, error) {
+	if err := gl.Init(); err != nil {
+		return nil, err
+	}
+
+	ctx := &Context{}
+	ctx.Framebuffer = &Framebuffer{
+		Object: 0, // Default framebuffer object.
+		ctx:    ctx,
+	}
+	return ctx, nil
+}
