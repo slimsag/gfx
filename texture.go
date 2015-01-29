114b487a82db82be1800652b2bb56f583ea66754
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-28 17:56:52 -0700

    all: add license headers

--- texture.go
+++ texture.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package gfx
 
 // Texture represents a single texture. It is used for images and cube maps
