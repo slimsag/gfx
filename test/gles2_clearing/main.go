494b7845b9234ecf8b80602e4bd026b26ae8bb51
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-29 11:02:00 -0700

    test/gles2_clearing: add standard OpenGL ES 2 clearing test.
    Build with -tags=gles2

---
+++ test/gles2_clearing/main.go
@@ -0,0 +1,51 @@
+// +build gles2
+
+package main
+
+import (
+	"runtime"
+
+	"github.com/slimsag/gfx"
+	"github.com/slimsag/gfx/debug"
+	"github.com/slimsag/gfx/gles2"
+	glfw "github.com/slimsag/glfw3"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize OpenGL 2 graphics context.
+	window.MakeContextCurrent()
+	gl, err := gles2.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// Wrap the context to get a debug context.
+	gl = debug.Context(gl)
+
+	for !window.ShouldClose() {
+		// Clear the color buffer to red.
+		gl.ClearColor(1, 0, 0, 1)
+		gl.Clear(gfx.ColorBuffer)
+
+		// Swap the front and back buffers, poll for events.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
