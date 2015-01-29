ef40e3204673b8cf76e97076e1059d23a4531f85
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-29 10:29:06 -0700

    internal/procaddr: add build-tagged procaddr package.

---
+++ internal/procaddr/auto/procaddr_egl.go
@@ -0,0 +1,15 @@
+// +build egl
+
+package auto
+
+// #cgo linux LDFLAGS: -lEGL
+// #include <stdlib.h>
+// #include <EGL/egl.h>
+import "C"
+import "unsafe"
+
+func GetProcAddress(name string) unsafe.Pointer {
+	var cname *C.char = C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	return unsafe.Pointer(C.eglGetProcAddress(cname))
+}
