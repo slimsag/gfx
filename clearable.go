0576904a0a32aea8309fae7cb0d51ae4a82d7f1c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-28 17:11:50 -0700

    Add Clearable interface.

---
+++ clearable.go
@@ -0,0 +1,33 @@
+package gfx
+
+// ClearMask represents a bitmask to choose which buffers to clear during a
+// framebuffer clearing operation. It must be one of the predefined constants.
+type ClearMask int
+
+// Clearing masks to select the color, depth, and stencil buffers. They can be
+// bitwise OR'd together to select multiple:
+//
+//  colorAndDepth := ColorBufer|DepthBuffer
+//
+const (
+	ColorBuffer ClearMask = iota
+	DepthBuffer
+	StencilBuffer
+)
+
+// Clearable represents the clearing state and API of a framebuffer object.
+type Clearable interface {
+	// ClearColor sets the color to clear the color buffer to upon a call to
+	// the Clear method.
+	ClearColor(r, g, b, a float32)
+
+	// ClearDepth sets the value to clear the depth buffer to upon a depth
+	// buffer clearing operation (a call to Clear with the DepthBuffer clear
+	// mask)
+	ClearDepth(depth float32)
+
+	// ClearStencil sets the value to clear the stencil buffer to upon a
+	// stencil buffer clearing operation (a call to Clear with the
+	// StencilBuffer clear mask)
+	ClearStencil(stencil int)
+}
