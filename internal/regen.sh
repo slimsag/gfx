18787367e9a2ab0d15abc30ffe8aea7168f20c63
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-03-23 11:46:42 -0700

    internal: update to use the latest Glow version (remove procaddr).

--- internal/regen.sh
+++ internal/regen.sh
@@ -6,12 +6,12 @@ pad() {
 
 echo "internal/gles2: regenerate Glow bindings"
 echo ""
-pad glow generate -api=gles2 -version=2.0 -restrict=./restrict.json
+pad glow generate -api=gles2 -version=2.0 -restrict=./restrict.json -out=./gles2/2.0/gles2
 pad goimports -w -l .
 pad patch --strip=1 < gles2.patch
 echo ""
 echo "internal/gl: regenerate Glow bindings"
 echo ""
-pad glow generate -api=gl -version=2.0 -restrict=./restrict.json
+pad glow generate -api=gl -version=2.0 -restrict=./restrict.json -out=./gl/2.0/gl
 pad goimports -w -l .
 pad patch --strip=1 < gl.patch
